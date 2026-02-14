f4ab5d4d6727008a856cebe6a90e549359f49918
OmkarArdekar12 <omkarardekar12@gmail.com>
2026-02-15 01:30:07 +0530

    Nearby Connectivity Section Completed

--- frontend/src/components/public/ConnectivitySection.jsx
+++ frontend/src/components/public/ConnectivitySection.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import API from "../../api/axios";
 import SectionLoader from "../common/SectionLoader";
+import { motion } from "framer-motion";
 
 export default function ConnectivitySection() {
   const [data, setData] = useState(null);
@@ -34,7 +35,11 @@ export default function ConnectivitySection() {
   }
 
   return (
-    <section
+    <motion.section
+      initial={{ opacity: 0, y: 50 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.8, ease: "easeOut" }}
+      viewport={{ amount: 0.25, once: true }}
       id="connectivity"
       className="w-full flex flex-col md:flex-row items-center justify-between py-4"
     >
@@ -55,6 +60,6 @@ export default function ConnectivitySection() {
           {data.description}
         </p>
       </div>
-    </section>
+    </motion.section>
   );
 }
