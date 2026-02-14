047114cc9f2bcd38cdd11d9606c2b0229213e8d4
OmkarArdekar12 <omkarardekar12@gmail.com>
2026-02-14 19:37:05 +0530

    Protected Route

--- frontend/src/routes/ProtectedRoute.jsx
+++ frontend/src/routes/ProtectedRoute.jsx
@@ -1,12 +1,13 @@
 import { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
+import FullScreenLoader from "../components/common/FullScreenLoader";
 
 const ProtectedRoute = ({ children }) => {
   const { isAdmin, loading } = useContext(AuthContext);
 
   if (loading) {
-    return <div>Loading...</div>;
+    return <FullScreenLoader />;
   }
 
   if (!isAdmin) {
