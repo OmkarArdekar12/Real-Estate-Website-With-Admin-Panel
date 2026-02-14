5f93029a4d00c9fd4d77225a98809c9bf43eaba1
OmkarArdekar12 <omkarardekar12@gmail.com>
2026-02-15 01:28:34 +0530

    About Section Completed

--- frontend/src/components/public/AboutSection.jsx
+++ frontend/src/components/public/AboutSection.jsx
@@ -39,7 +39,7 @@ export default function AboutSection() {
       initial={{ opacity: 0, y: 50 }}
       whileInView={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.8, ease: "easeOut" }}
-      viewport={{ amount: 0.2, once: true }}
+      viewport={{ amount: 0.25, once: true }}
       id="about"
       className="w-full flex flex-col md:flex-row items-center justify-between py-4"
     >
