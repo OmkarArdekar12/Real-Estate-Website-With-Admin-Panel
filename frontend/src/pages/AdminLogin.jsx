0f48fc25716997242cd965f5c405e2e280c37c95
OmkarArdekar12 <omkarardekar12@gmail.com>
2026-02-14 16:16:40 +0530

    Completed

--- frontend/src/pages/AdminLogin.jsx
+++ frontend/src/pages/AdminLogin.jsx
@@ -2,56 +2,72 @@ import { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 
-const AdminLogin = () => {
+export default function AdminLogin() {
   const { login } = useContext(AuthContext);
   const navigate = useNavigate();
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      await login(email, password);
-      navigate("/admin/dashboard");
-    } catch (err) {
-      setError("Invalid credentials");
-    }
+      const isAuthorized = await login(email, password);
+      if (isAuthorized) {
+        navigate("/admin/dashboard");
+      } else {
+      }
+    } catch (e) {}
   };
 
   return (
-    <div style={{ padding: "50px" }}>
-      <h2>Admin Login</h2>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 px-6">
+      <div className="bg-white shadow-xl rounded-lg w-full max-w-md p-10">
+        <h2 className="text-3xl font-serif font-bold text-center pb-8">
+          Admin Login
+        </h2>
 
-      <form onSubmit={handleSubmit}>
-        <div>
-          <input
-            type="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
+        <form onSubmit={handleSubmit} className="space-y-6">
+          <div>
+            <label className="block pl-1 text-sm font-medium" htmlFor="email">
+              Email
+            </label>
+            <input
+              id="email"
+              type="email"
+              className="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:border-yellow-400"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
 
-        <div>
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
+          <div>
+            <label
+              className="block pl-1 text-sm font-medium"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              id="password"
+              type="password"
+              className="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:border-yellow-400"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
 
-        {error && <p style={{ color: "red" }}>{error}</p>}
-
-        <button type="submit">Login</button>
-      </form>
+          <button
+            type="submit"
+            className="w-full bg-yellow-400 py-2 rounded-md font-semibold hover:bg-yellow-500 transition duration-300"
+          >
+            Login
+          </button>
+        </form>
+      </div>
     </div>
   );
-};
-
-export default AdminLogin;
+}
