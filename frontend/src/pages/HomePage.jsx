31adb4189b23288c44f74a9e5cfdc19c244e96c4
OmkarArdekar12 <omkarardekar12@gmail.com>
2026-02-14 20:22:09 +0530

    Home Page Completed

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -12,7 +12,7 @@ export default function HomePage() {
   return (
     <main className="w-full h-full pt-25 overflow-x-hidden">
       <HeroSection />
-      <div className="pt-15 pb-10 w-full flex flex-col gap-3 px-8 md:px-10 lg:px-20">
+      <div className="pt-15 pb-10 w-full flex flex-col gap-10 px-8 md:px-10 lg:px-20">
         <OverviewSection />
         <ConnectivitySection />
         <AmenitiesSection />
